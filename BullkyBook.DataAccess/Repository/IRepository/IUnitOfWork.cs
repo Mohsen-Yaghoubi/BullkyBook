ceb0a27a72f7cc293560f2c35834f22df75de916
Mohsen <max.yaghoubi@gmail.com>
2020-09-16 00:04:30 +0430

    Add Product Model And Product Repository

--- BullkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BullkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -8,6 +8,7 @@ namespace BullkyBook.DataAccess.Repository.IRepository
     {
         ICategoryRepository Category { get; }
         ICoverTypeRepository CoverType { get; }
+        IProductRepository Product { get; }
         void Save();
     }
 

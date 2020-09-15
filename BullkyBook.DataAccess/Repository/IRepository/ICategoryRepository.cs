b37810c91b2c0879d14b8e7633f58e09b55bbc07
Mohsen <max.yaghoubi@gmail.com>
2020-09-15 15:51:05 +0430

    Add project files.

---
+++ BullkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using BullkyBook.DataAccess.IRepository;
+using BullkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BullkyBook.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
---
+++ BullkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BullkyBook.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+    }
+}
